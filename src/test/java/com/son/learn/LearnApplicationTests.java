ce02be58568b78d4c7d2a2ded2f34bdb8963604f
Songu123 <vans72092@gmail.com>
2026-01-22 15:07:07 +0700

    feat: first commit

---
+++ src/test/java/com/son/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.son.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
